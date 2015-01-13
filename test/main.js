1f0c0c41baa51b5836a4521527a8d5fcf0e6538b
mobenar <tomas.enarsson@mobenga.com>
2015-01-13 14:50:29 +0100

    test case added

--- test/main.js
+++ test/main.js
@@ -1,6 +1,19 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 
+var TGF = require('../index');
+
 describe('TGF', function () {
+
+	it('should parse', function () {
+
+		var tgf = TGF.parse(fs.readFileSync('test/graph.tgf', 'utf8'));
+
+		var nodeIds = tgf.nodes.map(function (node) { return node.id; });
+		var edges = tgf.edges;
+
+		expect(nodeIds).to.have.members([1, 2, 3, 4, 5, 6, 7]);
+		expect(edges.length).to.equal(6);
+	});
 
 });
