cc71059b957b19280f56069612b6afa8d0226f11
mobenar <tomas.enarsson@mobenga.com>
2015-01-14 09:14:37 +0100

    test case for stringify added

--- test/main.js
+++ test/main.js
@@ -1,3 +1,5 @@
+/*global describe, before, it */
+
 var fs = require('fs');
 var expect = require('chai').expect;
 
@@ -5,15 +7,46 @@ var TGF = require('../index');
 
 describe('TGF', function () {
 
+	var tgf;
+
+	before(function () {
+
+		tgf = fs.readFileSync('test/graph.tgf', 'utf8').replace(/\r\n/g, '\n');
+	});
+
 	it('should parse', function () {
 
-		var tgf = TGF.parse(fs.readFileSync('test/graph.tgf', 'utf8'));
-
-		var nodeIds = tgf.nodes.map(function (node) { return node.id; });
-		var edges = tgf.edges;
+		var parsed = TGF.parse(tgf);
+		var nodeIds = parsed.nodes.map(function (node) { return node.id; });
+		var edges = parsed.edges;
 
 		expect(nodeIds).to.have.members([1, 2, 3, 4, 5, 6, 7]);
 		expect(edges.length).to.equal(6);
+	});
+
+	it('should stringify', function () {
+
+		var string = TGF.stringify({
+			nodes: [
+				{ id: 1, label: 'A' },
+				{ id: 2, label: 'B' },
+				{ id: 3, label: 'C' },
+				{ id: 4, label: 'D' },
+				{ id: 5, label: 'E' },
+				{ id: 6, label: 'F' },
+				{ id: 7, label: 'G' }
+			],
+			edges: [
+				{ source: 1, target: 2 },
+				{ source: 1, target: 3 },
+				{ source: 2, target: 4 },
+				{ source: 2, target: 5 },
+				{ source: 3, target: 6 },
+				{ source: 3, target: 7 }
+			]
+		});
+
+		expect(string).to.equal(tgf);
 	});
 
 });
